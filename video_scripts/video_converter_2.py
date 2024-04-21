d5097f4be5f693c9f448b5750dfd450a94937923
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-21 13:56:39 +0300

    Update video_converter_2.py

--- video_scripts/video_converter_2.py
+++ video_scripts/video_converter_2.py
@@ -1,28 +1,29 @@
 from moviepy.editor import VideoFileClip
-import os
+from pathlib import Path
+import logging
+import concurrent.futures
 
 class VideoConverter:
     def __init__(self, output_dir, video_format, output_video_format):
-        self.output_dir = output_dir
+        self.output_dir = Path(output_dir)
         self.video_format = video_format
         self.output_video_format = output_video_format
 
     def convert_video(self):
-        # Get a list of all subdirectories in the output directory
-        subdirectories = [d for d in os.listdir(self.output_dir) if os.path.isdir(os.path.join(self.output_dir, d))]
+        subdirectories = [d for d in self.output_dir.iterdir() if d.is_dir()]
 
-        for i, subdir in enumerate(subdirectories):
-            # Full path to the source video file
-            input_file = os.path.join(self.output_dir, f'input_video_{i}/input_video.{self.video_format}')
-            # Create a directory for the converted file
-            destination_dir = os.path.join(self.output_dir, f'input_conv_video_{i}')
-            os.makedirs(destination_dir, exist_ok=True)
-            # Full path to the output file
-            output_file = os.path.join(destination_dir, f'converted_video.{self.output_video_format}')
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(self.process_subdir, subdirectories)
 
-            try:
-                clip = VideoFileClip(input_file)
-                clip.write_videofile(output_file, codec='libx264')
-                print(f'File {input_file} successfully converted to {output_file}')
-            except Exception as e:
-                print(f'Error converting file {input_file}: {e}')
+    def process_subdir(self, subdir):
+        input_file = subdir / f'input_video.{self.video_format}'
+        destination_dir = self.output_dir / f'{subdir.name}_conv'
+        destination_dir.mkdir(exist_ok=True)
+        output_file = destination_dir / f'converted_video.{self.output_video_format}'
+
+        try:
+            clip = VideoFileClip(str(input_file))
+            clip.write_videofile(str(output_file), codec='libx264')
+            logging.info(f'File {input_file} successfully converted to {output_file}')
+        except Exception as e:
+            logging.error(f'Error converting file {input_file}: {e}', exc_info=True)
