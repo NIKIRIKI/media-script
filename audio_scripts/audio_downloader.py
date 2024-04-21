c0b9f47910e70e2cb8ed30cacd3bec50dc46823a
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-21 16:07:39 +0300

    Update audio_downloader.py

--- audio_scripts/audio_downloader.py
+++ audio_scripts/audio_downloader.py
@@ -1,23 +1,33 @@
-import os
 import subprocess
-import requests
+from pathlib import Path
+import concurrent.futures
+import logging
+
 
 class AudioDownloader:
     def __init__(self, urls, output_dir, yt_dlp_path, ffmpeg_path, audio_format='mp3', download_audio=True):
         self.urls = urls
-        self.output_dir = output_dir
+        self.output_dir = Path(output_dir)
         self.yt_dlp_path = yt_dlp_path
         self.ffmpeg_path = ffmpeg_path
         self.audio_format = audio_format
         self.download_audio_flag = download_audio
 
     def download_audio(self):
-        for i, url in enumerate(self.urls):
-            audio_dir = os.path.join(self.output_dir, f'input_audio_{i}')
-            os.makedirs(audio_dir, exist_ok=True)
-            filename = os.path.join(audio_dir, f'input_audio' + '.' + self.audio_format)
-            try:
-                command = [self.yt_dlp_path, url, '-x', '--audio-format', self.audio_format, '-o', filename]
-                subprocess.run(command, check=True)
-            except Exception as e:
-                print(f"Error in download_audio: {str(e)}")
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(self.process_url, self.urls, range(len(self.urls)))
+
+    def process_url(self, url, i):
+        audio_dir = self.output_dir / f'input_audio_{i}'
+        audio_dir.mkdir(parents=True, exist_ok=True)
+        filename = audio_dir / f'input_audio.{self.audio_format}'
+        if not filename.exists():
+            self.download_audio_from_url(url, filename)
+
+    def download_audio_from_url(self, url, filename):
+        command = [self.yt_dlp_path, url, '-x', '--audio-format', self.audio_format, '-o', str(filename)]
+        try:
+            subprocess.run(command, check=True)
+            logging.info(f'Successfully downloaded {url} to {filename}')
+        except Exception as e:
+            logging.error(f'Error downloading {url} to {filename}: {e}', exc_info=True)
