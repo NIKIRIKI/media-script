90be49e46a98f88d827c05e8922951f1be8d42ef
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-21 14:00:04 +0300

    Update video_downloader_2.py

--- video_scripts/video_downloader_2.py
+++ video_scripts/video_downloader_2.py
@@ -1,40 +1,37 @@
-import os
 import requests
+from pathlib import Path
+import concurrent.futures
+import logging
+
 
 class VideoDownloader:
     def __init__(self, urls, output_dir, video_format='mp4'):
         self.urls = urls
-        self.output_dir = output_dir
+        self.output_dir = Path(output_dir)
         self.video_format = video_format
 
     def download_content(self):
-        try:
-            for i, url in enumerate(self.urls):
-                self.download_url_content(url, i)
-        except Exception as e:
-            print(f"Error in download_content: {str(e)}")
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(self.download_url_content, self.urls, range(len(self.urls)))
 
     def download_url_content(self, url, i):
-        try:
-            url_dir = os.path.join(self.output_dir, f'input_video_{i}')
-            os.makedirs(url_dir, exist_ok=True)
-            video_filename = os.path.join(url_dir, f'input_video.{self.video_format}')
-            if not os.path.exists(video_filename):
-                self.download_video(url, video_filename)
-        except Exception as e:
-            print(f"Error in download_url_content: {str(e)}")
+        url_dir = self.output_dir / f'input_video_{i}'
+        url_dir.mkdir(parents=True, exist_ok=True)
+        video_filename = url_dir / f'input_video.{self.video_format}'
+        if not video_filename.exists():
+            self.download_video(url, video_filename)
 
     def download_video(self, url, filename):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
         try:
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-            }
             response = requests.get(url, stream=True, headers=headers)
-            if response.status_code == 200:
-                with open(filename, 'wb') as f:
-                    for chunk in response.iter_content(chunk_size=1024):
-                        if chunk:
-                            f.write(chunk)
+            response.raise_for_status()
+            with open(filename, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            logging.info(f'Successfully downloaded {url} to {filename}')
         except Exception as e:
-            print(f"Error in download_video: {str(e)}")
-
+            logging.error(f'Error downloading {url} to {filename}: {e}', exc_info=True)
