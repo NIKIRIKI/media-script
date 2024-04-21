c274e5dcddd4cd55c322a8ed3cd1bc39b6df6aac
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-21 16:07:21 +0300

    Update audio_converter.py

--- audio_scripts/audio_converter.py
+++ audio_scripts/audio_converter.py
@@ -1,60 +1,34 @@
 import os
 import subprocess
-from tqdm import tqdm
+from pathlib import Path
+import concurrent.futures
+import logging
 
-class AudioConverter:
-    def __init__(self, output_dir, ffmpeg_path, audio_format, output_audio_format):
-        self.output_dir = output_dir
+
+class AudioDownloader:
+    def __init__(self, urls, output_dir, yt_dlp_path, ffmpeg_path, audio_format='mp3', download_audio=True):
+        self.urls = urls
+        self.output_dir = Path(output_dir)
+        self.yt_dlp_path = yt_dlp_path
         self.ffmpeg_path = ffmpeg_path
         self.audio_format = audio_format
-        self.output_audio_format = output_audio_format
-        self.target_format = output_audio_format
-        self.sample_rate = 44100
-        self.bit_depth = 16
+        self.download_audio_flag = download_audio
 
-    def convert_audio(self):
-        # Check if the ffmpeg executable exists
-        if not os.path.exists(self.ffmpeg_path):
-            print(f"ffmpeg executable not found at {self.ffmpeg_path}. Please check the path.")
-            return
+    def download_audio(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(self.process_url, self.urls, range(len(self.urls)))
 
-        # Get a list of all directories in the output directory
-        directories = [d for d in os.listdir(self.output_dir) if os.path.isdir(os.path.join(self.output_dir, d))]
+    def process_url(self, url, i):
+        audio_dir = self.output_dir / f'input_audio_{i}'
+        audio_dir.mkdir(parents=True, exist_ok=True)
+        filename = audio_dir / f'input_audio.{self.audio_format}'
+        if not filename.exists():
+            self.download_audio_from_url(url, filename)
 
-        # Initialize progress bar
-        pbar = tqdm(total=len(directories), ncols=70)
-
-        for i in range(len(directories)):
-            # Full path to the source audio file
-            input_dir = os.path.join(self.output_dir, f'input_audio_{i}')
-            input_file = os.path.join(input_dir, f'input_audio.{self.audio_format}')
-            # Check if the input file exists
-            if not os.path.exists(input_file):
-                print(f"File {input_file} does not exist. Skipping...")
-                continue
-            # Create the destination directory
-            destination_dir = os.path.join(self.output_dir, f'input_conv_audio_{i}')
-            os.makedirs(destination_dir, exist_ok=True)
-            # Full path to the destination file
-            output_file = os.path.join(destination_dir, f'input_conv_audio.{self.output_audio_format}')
-            
-            # Conversion command
-            command = [
-                self.ffmpeg_path,
-                '-i', input_file,
-                '-ar', str(self.sample_rate),
-                '-ac', '2',  # Number of audio channels
-                '-sample_fmt', f's{self.bit_depth}',  # Sample depth
-                output_file
-            ]
-            try:
-                subprocess.run(command, check=True)
-                print(f'File {input_file} successfully converted to {output_file}')
-            except subprocess.CalledProcessError as e:
-                print(f'Error converting file {input_file}: {e}')
-
-            # Update progress bar
-            pbar.update(1)
-
-        # Close progress bar
-        pbar.close()
+    def download_audio_from_url(self, url, filename):
+        command = [self.yt_dlp_path, url, '-x', '--audio-format', self.audio_format, '-o', str(filename)]
+        try:
+            subprocess.run(command, check=True)
+            logging.info(f'Successfully downloaded {url} to {filename}')
+        except Exception as e:
+            logging.error(f'Error downloading {url} to {filename}: {e}', exc_info=True)
