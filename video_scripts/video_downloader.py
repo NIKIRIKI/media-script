a74110fef7ccfd33afee80b5b7d04ec36059f103
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-25 23:24:03 +0300

    Update video_downloader.py

--- video_scripts/video_downloader.py
+++ video_scripts/video_downloader.py
@@ -1,12 +1,11 @@
-import subprocess
+import yt_dlp
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
 class VideoDownloader:
-    def __init__(self, urls, output_dir, yt_dlp_path="yt-dlp-master/yt-dlp.exe"):
+    def __init__(self, urls, output_dir):
         self.urls = urls
         self.output_dir = Path(output_dir)
-        self.yt_dlp_path = yt_dlp_path
 
     def download_videos(self):
         with ThreadPoolExecutor() as executor:
@@ -17,5 +16,6 @@ class VideoDownloader:
         output_dir = self.output_dir / f'input_video_{i}'
         output_dir.mkdir(parents=True, exist_ok=True)
         output_file = output_dir / 'input_video.%(ext)s'
-        command = [self.yt_dlp_path, url, '-o', str(output_file)]
-        subprocess.run(command, check=True)
+        ydl_opts = {'outtmpl': str(output_file)}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
