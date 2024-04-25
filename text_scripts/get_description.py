af39a8ed977072ef134f0bec460403fbd42bf795
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-26 00:13:17 +0300

    Update get_description.py

--- text_scripts/get_description.py
+++ text_scripts/get_description.py
@@ -1,11 +1,10 @@
 import os
-import subprocess
+import yt_dlp
 
 class VideoDescriptionDownloader:
-    def __init__(self, urls, output_dir, yt_dlp_path):
+    def __init__(self, urls, output_dir):
         self.urls = urls
         self.output_dir = output_dir
-        self.yt_dlp_path = yt_dlp_path
 
     def download_all_descriptions(self):
         try:
@@ -19,14 +18,50 @@ class VideoDescriptionDownloader:
             url_dir = os.path.join(self.output_dir, f'input_description_{i}')
             os.makedirs(url_dir, exist_ok=True)
             self.download_description(url, url_dir)
+            self.download_title(url, url_dir)
+            self.download_subtitles(url, url_dir)
         except Exception as e:
             print(f"Error in download_url_content: {str(e)}")
 
     def download_description(self, url, url_dir):
         try:
-            command = [self.yt_dlp_path, url, '--get-description']
-            description = subprocess.check_output(command).decode('utf-8', 'ignore').strip()
-            with open(os.path.join(url_dir, 'input_description.txt'), 'w', encoding='utf-8') as f:
-                f.write(description)
+            ydl_opts = {
+                'skip_download': True,
+                'outtmpl': os.path.join(url_dir, 'description.txt')
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
+                description = info_dict.get('description', None)
+                with open(os.path.join(url_dir, 'description.txt'), 'w', encoding='utf-8') as f:
+                    f.write(description)
         except Exception as e:
             print(f"Error in download_description: {str(e)}")
+
+    def download_title(self, url, url_dir):
+        try:
+            ydl_opts = {
+                'skip_download': True,
+                'outtmpl': os.path.join(url_dir, 'title.txt')
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
+                title = info_dict.get('title', None)
+                with open(os.path.join(url_dir, 'title.txt'), 'w', encoding='utf-8') as f:
+                    f.write(title)
+        except Exception as e:
+            print(f"Error in download_title: {str(e)}")
+
+    def download_subtitles(self, url, url_dir):
+        try:
+            ydl_opts = {
+                'skip_download': True,
+                'writesubtitles': True,
+                'writeautomaticsub': True,
+                'subtitleslangs': ['en'],
+                'subtitlesformat': 'vtt',
+                'outtmpl': os.path.join(url_dir, 'subtitles.vtt')
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+        except Exception as e:
+            print(f"Error in download_subtitles: {str(e)}")
