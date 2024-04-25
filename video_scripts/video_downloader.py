639d23d29cc65325a7e232f50c59af93932189ac
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-25 23:15:23 +0300

    Update video_downloader.py

--- video_scripts/video_downloader.py
+++ video_scripts/video_downloader.py
@@ -1,51 +1,21 @@
-import requests
 import subprocess
-import logging
-import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
-
+from concurrent.futures import ThreadPoolExecutor
 
 class VideoDownloader:
-    def __init__(self, urls, output_dir, yt_dlp_path, ffmpeg_path, resolution='best', input_video_format='mp4', max_retries=3,
-                 max_workers=5):
+    def __init__(self, urls, output_dir, yt_dlp_path="yt-dlp-master/yt-dlp.exe"):
         self.urls = urls
         self.output_dir = Path(output_dir)
         self.yt_dlp_path = yt_dlp_path
-        self.ffmpeg_path = ffmpeg_path
-        self.resolution = resolution
-        self.input_video_format = input_video_format
-        self.max_retries = max_retries
-        self.max_workers = max_workers
-        logging.basicConfig(level=logging.INFO)
 
-    def download_content(self):
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            futures = {executor.submit(self.download_url_content, url, i): url for i, url in enumerate(self.urls)}
-            for future in as_completed(futures):
-                url = futures[future]
-                try:
-                    future.result()
-                except Exception as e:
-                    logging.error(f"Error downloading {url}: {str(e)}")
+    def download_videos(self):
+        with ThreadPoolExecutor() as executor:
+            for i, url in enumerate(self.urls):
+                executor.submit(self.download_video, url, i)
 
-    def download_url_content(self, url, i):
-        url_dir = self.output_dir / f'input_video_{i}'
-        url_dir.mkdir(parents=True, exist_ok=True)
-        video_filename = url_dir / f'input_video.{self.input_video_format}'
-        if not video_filename.exists():
-            self.download_video_with_retries(url, video_filename)
-
-    def download_video_with_retries(self, url, filename):
-        for i in range(self.max_retries):
-            try:
-                self.download_video(url, filename)
-                return
-            except (requests.exceptions.RequestException, subprocess.CalledProcessError) as e:
-                logging.error(f"Error in download_video: {str(e)}, retrying...")
-                time.sleep(i * 2)
-        logging.error(f"Failed to download video after {self.max_retries} attempts")
-
-    def download_video(self, url, filename):
-        command = [self.yt_dlp_path, url, '-f', f'{self.resolution}/{self.input_video_format}', '-o', str(filename)]
+    def download_video(self, url, i):
+        output_dir = self.output_dir / f'input_video_{i}'
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_file = output_dir / 'input_video.%(ext)s'
+        command = [self.yt_dlp_path, url, '-o', str(output_file)]
         subprocess.run(command, check=True)
