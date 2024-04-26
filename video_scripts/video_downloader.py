af6ce4e37cd0bf941e9f9a08c183e4b191fc72bc
NIKIRIKI <59776762+NIKIRIKI@users.noreply.github.com>
2024-04-26 18:51:15 +0300

    Update video_downloader.py

--- video_scripts/video_downloader.py
+++ video_scripts/video_downloader.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
 class VideoDownloader:
-    def __init__(self, urls, output_dir):
+    def __init__(self, urls, input_dir):
         self.urls = urls
-        self.output_dir = Path(output_dir)
+        self.input_dir = Path(input_dir)
 
     def download_videos(self):
         with ThreadPoolExecutor() as executor:
@@ -13,9 +13,9 @@ class VideoDownloader:
                 executor.submit(self.download_video, url, i)
 
     def download_video(self, url, i):
-        output_dir = self.output_dir / f'input_video_{i}'
-        output_dir.mkdir(parents=True, exist_ok=True)
-        output_file = output_dir / 'input_video.%(ext)s'
+        input_dir = self.input_dir / f'input_video_{i}'
+        input_dir.mkdir(parents=True, exist_ok=True)
+        output_file = input_dir / 'input_video.%(ext)s'
         ydl_opts = {'outtmpl': str(output_file)}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
